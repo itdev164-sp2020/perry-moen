75283d560917f80ad5268d7fd26b61cc4bef6760
Perry Moen <perrymoen@mm382-258911mac.local>
2020-01-30 15:33:06 -0600

    update

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV164 2020`,
+    author: `Perry Moen`,
+    contact: {
+      name:`Perry Moen`,
+      company:`Blogs inc.`,
+      address:`123 street`
+    }
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
