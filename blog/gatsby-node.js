ed756f16c1dbbf1fe9097abd65eb3ac2befb1524
Perry <Perrymoen@gmail.com>
2020-02-09 16:23:13 -0600

    adding content pages static sites generation

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -8,7 +8,7 @@
 const path = require(`path`);
 
 exports.createPages = ({graphql,actions}) => {
-  const {createPages} = actions;
+  const {createPage} = actions;
   return new Promise((resolve,reject)=>{
     graphql(`
       {
@@ -24,7 +24,7 @@ exports.createPages = ({graphql,actions}) => {
         if(result.errors){
           reject(result.errors);
         }
-        result.data.allContentfulBlogPost.edges.forEach(edge) =>{
+        result.data.allContentfulBlogPost.edges.forEach((edge) =>{
           createPage({
             path: edge.node.slug,
             component: path.resolve(`./src/templates/blog-post.js`),
