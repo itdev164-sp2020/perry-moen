46c6f74ab61098178ed3e58b165cdcf1263da1f0
Perry <Perrymoen@gmail.com>
2020-05-09 15:11:44 -0500

    Complete Activity 7

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -1,12 +1,15 @@
+import {Search} from 'styled-icons/Feather'
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import styled from 'styled-components'
 import {H1} from '../Heading'
+import {IconButton} from '../Button'
+import {Section} from '../Section'
 
 const Outer = styled.header`
 background : ${props =>props.theme.header.backgroundColor};
-margin-bottom: 1.45rem
+margin-bottom: 1.45rem;
 `
 const Inner = styled.div `
   margin:0 auto;
@@ -22,11 +25,18 @@ const StyledLink = styled(Link) `
 const Header = ({ siteTitle }) => (
   <Outer>
    <Inner>
+   <Section flex>
+   <Section width = {11/12}>
     <H1>
       <StyledLink to="/">
         {siteTitle}
       </StyledLink>
     </H1>
+    </Section>
+    <Section width = {1/12}>
+      <IconButton icon = {<Search />}/>
+    </Section>
+    </Section>
    </Inner>
   </Outer>
 )
